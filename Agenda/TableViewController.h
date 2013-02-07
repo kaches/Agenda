baaedc7c9c085e2243495afe4e141bad6469cce7
Javier Del Real <kaches@gmail.com>
2013-02-06 20:47:05 -0600

    Tableview added and gesture recognizer from Main view to table view

---
+++ Agenda/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Agenda
+//
+//  Created by Kaches on 2/6/13.
+//  Copyright (c) 2013 Kaches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
